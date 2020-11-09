8fffa90fabe9e30d52c25cc40d0f673cb44dff5c
Julien Fraichot <jfraichot@learningmachine.com>
2020-11-09 17:03:46 +0100

    test(MerkleProof2019): test CBOR decode

--- tests/MerkleProof2019.test.ts
+++ tests/MerkleProof2019.test.ts
@@ -1,9 +1,23 @@
 import { MerkleProof2019 } from '../src/MerkleProof2019';
 import fixtureProof from './fixtures/proof';
+import assertionProof from './assertions/proof';
 
 describe('MerkleProof2019 test suite', function () {
-  it('works', function () {
-    const instance = new MerkleProof2019({ proof: fixtureProof });
+  let instance;
+
+  beforeEach(function () {
+    instance = new MerkleProof2019({ proof: fixtureProof });
+  });
+
+  afterEach(function () {
+    instance = null;
+  });
+
+  it('registers the type of the proof', function () {
     expect(instance.type).toBe('MerkleProof2019');
+  });
+
+  it('decodes the CBOR encoded proofValue', function () {
+    expect(instance.proof).toEqual(assertionProof);
   });
 });
