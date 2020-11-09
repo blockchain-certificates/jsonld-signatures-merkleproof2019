88e2ec692d57fb5e3e7bea6f9b844e2f279575e9
Julien Fraichot <jfraichot@learningmachine.com>
2020-11-09 14:14:40 +0100

    refactor(MerkleProof2019): convert to TS and cleanup unused API

--- src/MerkleProof2019.ts
+++ src/MerkleProof2019.ts
@@ -1,46 +1,41 @@
-'use strict';
-const jsigs = require('jsonld-signatures');
-const { Certificate } = require('@blockcerts/cert-verifier-js/lib');
+import { Certificate } from '@blockcerts/cert-verifier-js/lib';
+import { Decoder } from '@vaultie/lds-merkle-proof-2019';
+import jsigs from 'jsonld-signatures';
 const { LinkedDataProof } = jsigs.suites;
 
-module.exports = class MerkleProof2019 extends LinkedDataProof {
+export class MerkleProof2019 extends LinkedDataProof {
   /**
    * @param type {string} Provided by subclass.
-   *
-   * One of these parameters is required to use a suite for signing:
-   *
-   * @param [creator] {string} A key id URL to the paired public key.
+   * @param [issuer] {string} A key id URL to the paired public key.
    * @param [verificationMethod] {string} A key id URL to the paired public key.
-   *
-   * Advanced optional parameters and overrides:
-   *
    * @param [proof] {object} a JSON-LD document with options to use for
    *   the `proof` node (e.g. any other custom fields can be provided here
    *   using a context different from security-v2).
-   * @param [date] {string|Date} signing date to use if not passed.
-   * @param [useNativeCanonize] {boolean} true to use a native canonize
-   *   algorithm.
    * @param [blockcertsDocument] {BlockcertsV3 document} document used and signed by the MerkleProof2019 signature
    */
+  public type: string = 'MerkleProof2019';
+  public issuer: any = null; // TODO: define issuer type
+  public verificationMethod: string = '';
+  public proof: any = null; // TODO: define proof type
+
   constructor({
-                type = 'MerkleProof2019', creator, verificationMethod, proof, date,
-                useNativeCanonize, blockcertsDocument} = {}) {
+    type = 'MerkleProof2019',
+    issuer = null,
+    verificationMethod = '',
+    proof = null,
+    blockcertsDocument = null
+  } = {}) {
+    super({type});
     // validate common options
     if(verificationMethod !== undefined &&
       typeof verificationMethod !== 'string') {
       throw new TypeError('"verificationMethod" must be a URL string.');
     }
-    super({type});
-    this.creator = creator;
+    this.issuer = issuer;
     this.verificationMethod = verificationMethod;
-    this.proof = proof;
+    const base58Decoder = new Decoder(proof.proofValue);
+    this.proof = base58Decoder.decode();
     this.blockcertsCertificate = new Certificate(blockcertsDocument);
-    if(date !== undefined) {
-      this.date = new Date(date);
-      if(isNaN(this.date)) {
-        throw TypeError(`"date" "${date}" is not a valid date.`);
-      }
-    }
   }
 
   async verifyProof () {
@@ -51,4 +46,4 @@ module.exports = class MerkleProof2019 extends LinkedDataProof {
       verificationStatus
     };
   }
-};
+}
