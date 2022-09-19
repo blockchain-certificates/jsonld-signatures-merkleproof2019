78891772e11a7e37b5b396818fc0cbffa058268c
Julien Fraichot <julien.fraichot@hyland.com>
2022-09-19 15:23:23 +0200

    test(Contract): precompile vcjs library to cjs to prevent module errors

--- tests/contract/vc.js.contract.test.ts
+++ tests/contract/vc.js.contract.test.ts
@@ -1,8 +1,27 @@
-import vcjs from '@digitalbazaar/vc';
 import { preloadedContexts } from '@blockcerts/schemas';
+import { rollup } from 'rollup';
+import cjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
 import jsonld from 'jsonld';
+import { Headers } from 'node-fetch';
+import nodeEval from 'node-eval';
 import { MerkleProof2019 } from '../../src/MerkleProof2019';
 import blockcertsDocument from '../fixtures/testnet-v3-did.json';
+
+async function precompileVc (): Promise<string> {
+  return await rollup({
+    input: 'node_modules/@digitalbazaar/vc/lib/index.js',
+    plugins: [
+      resolve({
+        preferBuiltins: true
+      }),
+      cjs()
+    ]
+  }).then(async bundle => await bundle.generate({ format: 'cjs', inlineDynamicImports: true }))
+    .then(generated => {
+      return generated.output[0].code;
+    });
+}
 
 function generateDocumentLoader (): any {
   const customLoader = function (url): any {
@@ -21,11 +40,21 @@ function generateDocumentLoader (): any {
 describe('Contract test suite', function () {
   describe('vc.js compatibility', function () {
     it('should verify a MerkleProof2019 signed document', async function () {
+      const vcjsString = await precompileVc();
+      const vcjs = nodeEval(vcjsString, './index.js', {
+        globalThis: {
+          Headers
+        }
+      });
+      const presentation = vcjs.createPresentation({
+        verifiableCredential: [blockcertsDocument]
+      });
       const suite = [new MerkleProof2019({ document: blockcertsDocument })];
       const verificationStatus = await vcjs.verify({
-        presentation: blockcertsDocument,
+        presentation,
         suite,
-        documentLoader: generateDocumentLoader()
+        documentLoader: generateDocumentLoader(),
+        challenge: Math.random().toString().substr(2, 8)
       });
       console.log(verificationStatus);
       expect(verificationStatus.verified).toBe(true);
