e5e57736ef9974fa439e212570289f10eba87cda
Julien Fraichot <julien.fraichot@hyland.com>
2024-02-27 17:50:46 +0100

    feat(DataIntegrityProof): enable verification of data integrity proof

---
+++ tests/verification/data-integrity-proof-support.test.ts
@@ -0,0 +1,19 @@
+import fixture from '../fixtures/mocknet-vc-v2-data-integrity-proof.json';
+import { LDMerkleProof2019 } from '../../src';
+
+describe('given the documnet is signed following the DataIntegrityProof spec', function () {
+  describe('and is a valid MerkleProof2019 signature', function () {
+    it('should verify successfully', async function () {
+      const instance = new LDMerkleProof2019({
+        document: fixture,
+        proof: fixture.proof // merkle proof
+      });
+      const result = await instance.verifyProof();
+      expect(result).toEqual({
+        verified: true,
+        verificationMethod: null,
+        error: ''
+      });
+    });
+  });
+});
