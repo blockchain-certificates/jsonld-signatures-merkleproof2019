88b7b738c3da7a29bfa3f740e57ce23d51f59940
Julien Fraichot <julien.fraichot@hyland.com>
2025-06-23 17:49:13 +0200

    fix(Node): json file assertion

--- src/constants/contexts/contexts-list.ts
+++ src/constants/contexts/contexts-list.ts
@@ -1,7 +1,7 @@
-import BLOCKCERTSV2_ALPHA from './blockcerts.v2-alpha.json';
-import BLOCKCERTSV3_ALPHA from './blockcerts.v3-alpha.json';
-import BLOCKCERTSV3_BETA from './blockcerts.v3-beta.json';
-import ED25519 from './ed25519Context.json';
+import BLOCKCERTSV2_ALPHA from './blockcerts.v2-alpha.json' with { type: 'json' };
+import BLOCKCERTSV3_ALPHA from './blockcerts.v3-alpha.json' with { type: 'json' };
+import BLOCKCERTSV3_BETA from './blockcerts.v3-beta.json' with { type: 'json' };
+import ED25519 from './ed25519Context.json' with { type: 'json' };
 
 export default {
   BLOCKCERTSV2_ALPHA,
